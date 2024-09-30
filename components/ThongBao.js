2e167ed39c4eb1faf78f64376b142890eaa5ab22
Vo-Hoang-Minh-Sang <sangvo0339958048@gmail.com>
2024-09-30 19:30:11 +0700

    thêm màn hình thông báo

--- components/ThongBao.js
+++ components/ThongBao.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const ThongBao = () => {
     const navigation = useNavigation();
 
+    const notifications = [
+        { id: '1', message: 'Buổi học ngày 22 tháng 9 từ 12h30 - 15h: Đi học đúng giờ' },
+        { id: '2', message: 'Buổi học ngày 23 tháng 9 từ 14h00 - 16h30: Vắng' },
+        // Thêm các thông báo khác tương tự
+    ];
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Thông Báo!</Text>
+        <View style={styles.Container}>
+            <Text style={styles.textHeader}>Thông Báo</Text>
+            <FlatList
+                data={notifications}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <View style={styles.notificationContainer}>
+                        <Text style={styles.notificationText}>{item.message}</Text>
+                    </View>
+                )}
+            />
         </View>
-      );
+    );
 };
 
 export default ThongBao;
@@ -24,30 +39,19 @@ const styles = StyleSheet.create({
     textHeader: {
         fontWeight: 'bold',
         fontSize: 40,
-        marginBottom: 10
+        marginBottom: 10,
+        textAlign: 'center',
     },
-    text: {
-        fontSize: 20,
-        marginBottom: 20
-    },
-    optionContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        padding: 10,
+    notificationContainer: {
+        padding: 15,
         borderWidth: 1,
         borderColor: '#000',
         borderRadius: 10,
         marginBottom: 15,
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
     },
-    optionText: {
+    notificationText: {
         fontSize: 18,
         fontWeight: '500',
-    },
-    optionImage: {
-        width: 70,
-        height: 70,
-        resizeMode: 'contain',
     },
 });
